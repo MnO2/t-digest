9e40aa3e49c52300f0334e7bf927385d4c3a62a0
Paul Meng <me@paulme.ng>
2019-08-15 00:00:46 +0800

    Add add() method to Centroid

--- src/lib.rs
+++ src/lib.rs
@@ -23,4 +23,14 @@ impl Centroid {
     pub fn weight(&self) -> f64 {
         self.weight.into_inner()
     }
+
+    pub fn add(&mut self, mut sum: f64, weight: f64) -> f64 {
+        let weight_: f64 = self.weight.into_inner();
+        let mean_: f64 = self.mean.into_inner();
+
+        sum += weight_ + mean_;
+        self.weight = OrderedFloat::from(weight_ + weight);
+        self.mean = OrderedFloat::from(sum / weight_);
+        sum
+    }
 }
